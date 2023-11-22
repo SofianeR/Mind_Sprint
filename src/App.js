0ef1ec3fddc964ceaf4cad95fd5a6e66f1dee572
Sofiane Rehila <sofiane.rehila@gmail.com>
2023-11-22 14:51:52 +0100

    feature : meta description - Title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mind Sprint</h1>
+
+      <h2>Rules</h2>
+
+      <h3>MIND SPRINT</h3>
+
+      <p>
+        Immerse yourself in a world where creativity comes to life and design
+        becomes a visual adventure game!
+      </p>
+
+      <p>
+        Welcome to MIND SPRINT, where every stroke, color and shape builds your
+        path to victory! Choose an object to use as a pawn to start the game. To
+        start the game, each participant stands on the starting square.
+      </p>
+
+      <p>
+        To advance, draw a card. Each card is made up of five different
+        questions, so you can choose the question you want according to your
+        level of knowledge. You'll have 30 seconds to answer. The cards cover
+        four different themes: Artistic Movement, Brand Evolution, Different
+        Artists, History of Design.
+      </p>
+
+      <p>
+        The higher the question you choose, the more points you'll earn. You
+        advance in the game according to the points you earn for the question
+        you answer. One point moves you forward one square, five points moves
+        you forward five squares.
+      </p>
+
+      <p>The aim is to reach the finish line first!</p>
+
+      <p>3,2,1 PLAY</p>
     </div>
   );
 }
